44f4325eea03d4bfa20d1f21a6e6cae3ab3a433c
Le Thanh Trung <t000792@pc-000792-1.local>
2017-12-10 18:15:39 +0700

    hello
    commit-1

---
+++ hello.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<!-- Top Nav Bar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <a href="index.php" class="nav-brand" >TrungLe's Boutique</a>
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="https://www.google.com.vn/">Shirt</a></li>
+                    <li><a href="#">Pants</a></li>
+                    <li><a href="#">Shoes</a></li>
+                    <li><a href="#">Accessories</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<!-- Header
+<div id="headerWrapper">
+  <div id="back-flower"></div>
+  <div id="logotext"></div>
+  <div id="for-flower"></div>
+</div> -->
+</body>
+</html>
